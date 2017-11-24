7a92a926c6f8c3bbf57372dd8ff3d45645dbd107
Unknown <kessalin2560@gmail.com>
2017-11-24 13:52:32 +0700

    ข้อที่ 1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,13 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+  cout << "ms." << endl;
+  cout << "kessalin" << endl;
+  cout << "tosiri" << endl;
+  cout << "ked" << endl;
+  cout << "female" << endl;
+  cout << "28/03/2542" << endl;
+  cout << "play to game" << endl;
+  cout << "single" << endl;
   return 0;
 }
